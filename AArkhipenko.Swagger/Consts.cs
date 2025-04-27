f7b16fc39330c9e8b91dfd4b4fec9276fa55c752
AArkhipenko <arkhipenko.alex95@gmail.com>
2025-04-27 19:09:58 +0500

    AKP-20: Создание проекта
    Создание проекта nuget-пакета
    Создание методов расширения
    Создание проекта с тестами
    Настройка для публикации nuget-пакета
    REDAME

---
+++ AArkhipenko.Swagger/Consts.cs
@@ -0,0 +1,18 @@
+﻿namespace AArkhipenko.Swagger
+{
+    /// <summary>
+    /// Константы
+    /// </summary>
+    internal class Consts
+    {
+        /// <summary>
+        /// Ключ для поиска названия сервиса в переменных окружения
+        /// </summary>
+        public static string ServiceName => "SERVICE_NAME";
+
+        /// <summary>
+        /// Префикс для Swagger
+        /// </summary>
+        public static string SwaggerPrefix => "doc-api";
+    }
+}
